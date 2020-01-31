2fe3087e714eef3fa2ba227073505a3dc1f37cb3
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-01-30 20:37:13 -0500

    first commit

---
+++ sauti-studio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <SignUp />
+      
+     
+    </div>
+  );
+}
+
+export default App;
