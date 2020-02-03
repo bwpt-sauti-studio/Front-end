33bb848b3a072e066600b3954366eb7fe9af7baa
sophiasagan <sophiabraddock82@gmail.com>
2020-02-03 17:32:47 -0500

    contact form needs more styling

--- sauti-studio/src/Components/Contact.js
+++ sauti-studio/src/Components/Contact.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 import { useForm } from "react-hook-form";
 import './Contact.css';
 
+const defaultValues = {
+    Firstname: "",
+    Lastname: "",
+    Email: "",
+    Message: "",
+    Checkbox: false,
+    // switch: false,
+    // RadioGroup: ""
+  };
+
 const Contact = () => {
-    const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, watch, reset, errors } = useForm({ defaultValues });
     const onSubmit = data => console.log(data);
     console.log(errors);
 
@@ -12,11 +22,21 @@ const Contact = () => {
     <div className='form-wrapper'>
         <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Get In Touch</h1>
-        <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
+        <div className="form-group">
+        <input type="text" placeholder="First name" name="Firstname" ref={register({required: true, maxLength: 80})} />
         {errors.Firstname && <p>This field is required</p>}
-        <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
+        </div>
+
+        <div className="form-group">
+        <input type="text" placeholder="Last name" name="Lastname" ref={register({required: true, maxLength: 100})} />
+        </div>
+        
+        <div className="form-group">
         <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
         {errors.Email && <p>This field is required</p>}
+        </div>
+
+        <div className="form-group">
         <select name="Subject" ref={register({ required: true })}>
         <option value="App Support">App Support</option>
         <option value=" My Account"> My Account</option>
@@ -24,14 +44,43 @@ const Contact = () => {
         <option value=" Just Saying Hi!"> Just Saying Hi!</option>
         </select>
         {errors.Subject && <p>This field is required</p>}
-        <textarea name="Message" ref={register({required: true, min: 8})} />
+        </div>
+
+        <br/>
+
+        <div className="form-group">
+        <textarea placeholder="Message" name="Message" rows="9" ref={register({required: true, min: 8})} />
         {errors.Message && <p>This field is required</p>}
+        </div>
 
-      <input name="Subscriber" type="radio" value="Yes" ref={register({ required: true })}/>
-      <input name="Subscriber" type="radio" value="No" ref={register({ required: true })}/>
+        <br/>
 
-      
-      <input type="submit" />
+      {/* <input name="Subscriber" type="radio" value="Yes" ref={register({ required: true })}/>
+      <input name="Subscriber" type="radio" value="No" ref={register({ required: true })}/> */}
+
+      <div className="form-group">
+            <input
+              type="checkbox"
+              placeholder="Subscribe to Newsletter"
+              name="Subscribe to Newsletter"
+              id="customCheck1"
+              ref={register}
+            />
+            <label htmlFor="customCheck1"> Subscribe to Newsletter</label>
+          </div>
+
+          <button
+          type="button"
+          onClick={() => {
+            reset(defaultValues);
+          }}
+        >
+          Reset Form
+        </button>
+
+        <div className="form-group">
+            <input type="submit" />
+            </div>
 
       {/* <button type="submit">Send Message</button> */}
     </form>
