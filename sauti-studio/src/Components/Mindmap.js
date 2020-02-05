670061c9d85099cb8a71a377dbde416c1b0fda4f
sophiasagan <sophiabraddock82@gmail.com>
2020-02-05 09:21:30 -0500

    NavBar and Contact done

--- sauti-studio/src/Components/Mindmap.js
+++ sauti-studio/src/Components/Mindmap.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import SortableTree, {toggleExpandedForAll, removeNodeAtPath } from 'react-sortable-tree';
-import 'react-sortable-tree/style.css';
-import "./styles.css";
+// import React, { Component } from 'react';
+// import SortableTree, {toggleExpandedForAll, removeNodeAtPath } from 'react-sortable-tree';
+// import 'react-sortable-tree/style.css';
+// import "./styles.css";
 
-const maxDepth = 5;
+// const maxDepth = 5;
 
 // const alertNodeInfo = ({ node, path, treeIndex }) => {
 //     const objectString = Object.keys(node)
@@ -20,58 +20,58 @@ const maxDepth = 5;
 
 
 
-export default class Mindmap extends React.Component {
-    counter = 0;
-    constructor(props) {
-      super(props);
+// export default class Mindmap extends React.Component {
+//     counter = 0;
+//     constructor(props) {
+//       super(props);
   
-      this.state = {
-        name: "",
-        treeData: [
-          { title: "Chicken", expanded: true, children: [{ title: "Egg" }] }
-        ]
-      };
-    }
+//       this.state = {
+//         name: "",
+//         treeData: [
+//           { title: "Chicken", expanded: true, children: [{ title: "Egg" }] }
+//         ]
+//       };
+//     }
   
-    handleChange(e) {
-      this.setState({ name: e.target.value });
-    }
+//     handleChange(e) {
+//       this.setState({ name: e.target.value });
+//     }
   
-    addNewLabel() {
-      this.counter += 1;
-      this.setState({
-        treeData: this.state.treeData.concat({
-          title: this.state.name ? this.state.name : `test-${this.counter}`,
-          expanded: true
-        })
-      });
-      this.setState({ name: "" });
-    }
-    canDrag(e) {
-      return e.node.title && e.node.title.includes("test");
-    }
+//     addNewLabel() {
+//       this.counter += 1;
+//       this.setState({
+//         treeData: this.state.treeData.concat({
+//           title: this.state.name ? this.state.name : `test-${this.counter}`,
+//           expanded: true
+//         })
+//       });
+//       this.setState({ name: "" });
+//     }
+//     canDrag(e) {
+//       return e.node.title && e.node.title.includes("test");
+//     }
   
-    render() {
-      return (
-        <div style={{ height: 500 }}>
-          <div style={{ width: 100 }}>
-            {" "}
-            {/* <label>Data: {JSON.stringify(this.state.treeData)}</label> */}
-          </div>
+//     render() {
+//       return (
+//         <div style={{ height: 500 }}>
+//           <div style={{ width: 100 }}>
+//             {" "}
+//             {/* <label>Data: {JSON.stringify(this.state.treeData)}</label> */}
+//           </div>
   
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange.bind(this)}
-          />
-          <button onClick={() => this.addNewLabel()}>Add</button>
-          <SortableTree
-            // canDrag={this.canDrag}
-            treeData={this.state.treeData}
-            onChange={treeData => this.setState({ treeData })}
-          />
-        </div>
-      );
-    }
-  }
+//           <input
+//             type="text"
+//             value={this.state.name}
+//             onChange={this.handleChange.bind(this)}
+//           />
+//           <button onClick={() => this.addNewLabel()}>Add</button>
+//           <SortableTree
+//             // canDrag={this.canDrag}
+//             treeData={this.state.treeData}
+//             onChange={treeData => this.setState({ treeData })}
+//           />
+//         </div>
+//       );
+//     }
+//   }
   
--- sauti-studio/src/Components/NavBar.js
+++ sauti-studio/src/Components/NavBar.js
@@ -17,7 +17,7 @@ import './NavBar.css';
             <a id="about" className="menu-item" href="/about">About</a>
             <a id="signup" className="menu-item" href="/signup">Sign Up</a>
             <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a id="mindmap" className="menu-item" href="/mindmap">Mindmap</a>
+            {/* <a id="mindmap" className="menu-item" href="/mindmap">Mindmap</a> */}
             {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
             {/* <button className={'btn btn-primary'} style={{position: "fixed", bottom: "0"}}>Start New</button> */}
           </Menu>
