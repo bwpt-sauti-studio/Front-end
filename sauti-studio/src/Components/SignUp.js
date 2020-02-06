3e988551449b457a64236d99601e45072ffae7d1
sophiasagan <sophiabraddock82@gmail.com>
2020-02-05 21:53:31 -0500

    update signup formatting

--- sauti-studio/src/Components/SignUp.js
+++ sauti-studio/src/Components/SignUp.js
@@ -1,50 +1,27 @@
-
-
-import React from 'react'
+import React, { useState } from "react";
+import React from 'react';
 import { Link } from 'react-router';
-
 import Login from './Login';
-
 import '../Components/../App.css';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-
-
 // export default function SignUp() {
   function SignUp(){
-
-
-
     return (
-
-
       <div className="whole-page">
-        
         <div className="sign-up-section">
-          
                 <h1 className="title"> Create an account </h1>
                 <Form >
-            
              <Field type="text" name="first-name" placeholder="First Name" />
-        
              <Field type="text" name="last-name" placeholder="Last Name" />
-          
              <Field type="email" name="email" placeholder="Email" />
-          
              <Field type="text" name="username" placeholder="Username" />
-          
             <Field type="password" name="password" placeholder="Password" />
-        
             <button>Create Account </button>
-            
             <p className="last-text">Already have an Account?
             <a href="Components/Login.js"> Please Sign in </a> </p> 
-
                 </Form>
-
-
             </div>
-
             <nav className ="thierno-nav">
                 <p className ="footer-text">Copyright © bw-sauti-studio</p>
             </nav>
