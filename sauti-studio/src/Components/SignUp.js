484a25dcaf53c31bad2c9ce30df406f4e75aee2d
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-02-07 11:33:56 -0500

    completed project

--- sauti-studio/src/Components/SignUp.js
+++ sauti-studio/src/Components/SignUp.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../Components/../App.css';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
@@ -7,10 +7,17 @@ import axios from "axios";
 
 
 // export default function SignUp() {
-  function SignUp(){
-
-
-
+const  SignUp = ({
+  values,
+  errors,
+  touched,
+  status
+}) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    console.log('status has changed', status);
+  
+  }, [status]);
     return (
 
       <div className="whole-page">
@@ -20,15 +27,36 @@ import axios from "axios";
           <h1 className="title"> Create an account </h1>
           <Form >
             
-            <Field type="text" name="first-name" placeholder="First Name" />
+            <Field id="firstname" type="text" name="firstname" placeholder="First Name" />
+            {touched.firstname &&
+              errors.firstname && (
+              <p className ="errors">{errors.firstname}</p>
+            )}
         
-            <Field type="text" name="last-name" placeholder="Last Name" />
+            <Field id="lastname" type="text" name="lastname" placeholder="Last Name" />
+            {touched.lastname &&
+              errors.lastname && (
+              <p className ="errors">{errors.lastname}</p>
+            )}
           
-            <Field type="email" name="email" placeholder="Email" />
+            <Field id="email" type="email" name="email" placeholder="Email" />
+            
+            {touched.email &&
+              errors.email && (
+              <p className ="errors">{errors.email}</p>
+            )}
           
-            <Field type="text" name="username" placeholder="Username" />
+            <Field id="username" type="text" name="username" placeholder="Username" />
+            {touched.username &&
+              errors.username && (
+              <p className ="errors">{errors.username}</p>
+            )}
           
-            <Field type="password" name="password" placeholder="Password" />
+            <Field id="password" type="password" name="password" placeholder="Password" />
+            {touched.password &&
+              errors.password && (
+              <p className ="errors">{errors.password}</p>
+            )}
         
             <button type="submit">Create Account </button>
             
@@ -48,8 +76,10 @@ import axios from "axios";
     );
 }
 const FormikSignUp = withFormik({
-  mapPropsToValues({ email, username, password }) {
+  mapPropsToValues({firstname, lastname, email, username, password }) {
     return {
+      firstname: firstname || "",
+      lastname: lastname || "",
       email: email || "",
       username: username || "",
       password: password || ""
@@ -59,6 +89,9 @@ const FormikSignUp = withFormik({
 
 
   validationSchema: Yup.object().shape({
+    firstname: Yup.string("First name must be a string").required("First Name is a required field"),
+    
+     lastname: Yup.string("Last name must be a string").required("Last name is a required field"),
     email: Yup.string()
       .email("Email not valid")
       .required("Email is required"),
@@ -66,37 +99,28 @@ const FormikSignUp = withFormik({
       .required()
       .min(6),
     password: Yup.string()
-      .min(16, "Password must be 16 characters or longer")
+      .min(8, "Password must be 12 characters or longer")
       .required("Password is required")
   }),
 
-  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-    if (values.email === "alreadytaken@atb.dev") {
-      setErrors({ email: "That email is already taken" });
-    } else {
+  // handleSubmit(values, formikBag, { resetForm, setErrors, setSubmitting }) {
+  handleSubmit(values, { setStatus } ) {
+    console.log("submitting", values);
       axios
-        .post("'https://build-iv-sauti-studio.herokuapp.com/signup")
+        .post("https://build-iv-sauti-studio.herokuapp.com/sigup")
         
         .then(res => {
-          console.log(res); 
-          resetForm();
-          setSubmitting(false);
+          console.log("Success:", res); 
+          setStatus(res.data);
+          // resetForm();
+          // setSubmitting(false);
         })
         .catch(err => {
           console.log(err);  
-          setSubmitting(false);
+          // setSubmitting(false);
         });
     }
-  }
-
+  
 })(SignUp);
 export default FormikSignUp;
-
-//  const [credentials, setCredentials] = useState({ username: '', password: ''});
-
-//     const login = e => {
-//         e.preventDefault();
-//         axiosWithAuth()
-//         .post('https://build-iv-sauti-studio.herokuapp.com/login', credentials)
-//         .then(res => {
 
